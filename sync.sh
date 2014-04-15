45f4fb4b4937777cf0519448d99acee3f6f6a6d2
Isaac Cambron <isaac@isaaccambron.com>
2014-04-15 10:35:51 -0400

    converted sync to a bash script, this time with the actual file

---
+++ sync.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+shopt -s dotglob
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for f in *
+do
+  if [[ $f =~ \.(rb|sh)$ ]] || [[ $f =~ ^\.git ]]; then
+    continue
+  fi
+
+  dest=$f
+
+  if [[ $f =~ ^renamed-(.*) ]]; then
+    dest=${BASH_REMATCH[1]}
+  fi
+
+  echo "Linking $f to ~/$dest"
+  ln -sfn $dir/$f ~/$dest
+
+done
+
+vim +BundleInstall +qall
+cd .vim/bundle/vimproc.vim; make
