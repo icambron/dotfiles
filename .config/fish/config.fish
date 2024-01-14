87671d1b48486a1e7f5547a9a7d59c496d0c0aa9
Isaac Cambron <icambron@gmail.com>
2024-01-14 04:24:02 -0500

    various

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,7 @@ fish_vi_key_bindings
 
 # PROMPT
 if type -q starship
-  starship init fish | source
+    starship init fish | source
 end
 
 # PATHS
@@ -15,21 +15,22 @@ fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/code/lqbk/LBDeploy/src/main/python
 fish_add_path /opt/homebrew/bin
-fish_add_path /opt/homebrew/opt/ruby/bin    
+fish_add_path /opt/homebrew/opt/ruby/bin
 fish_add_path (ruby -e 'puts Gem.dir')/bin
 
 # OTHER ENV
 export EDITOR=nvim
 export SUDO_EDITOR=nvim
 export GRIM_DEFAULT_DIR=$HOME/screenshots
+export GPG_TTY=(tty)
 
 if test -e ~/.private_fish
-	source ~/.private_fish
+    source ~/.private_fish
 end
 
 # ALIASES
 alias less='less -i'
-alias weather='curl http://wttr.in/Boston'
+alias weather='curl http://wttr.in/lexington+ma'
 alias python=python3
 alias pip=pip3
 alias python=python3
@@ -40,45 +41,46 @@ alias z=zoxide
 alias zel=zellij
 
 if type -q nvim
-  alias vim=nvim
+    alias vim=nvim
 end
 
 if type -q tre
-  alias tree=tre
+    alias tree=tre
 end
 
 if type -q exa
-  alias ls=exa
-  alias exa="exa --header --git --all"
+    alias lss=/bin/ls
+    alias ls=exa
+    alias exa="exa --header --git --all"
 else
-  alias ls='ls -aG --color=auto'
+    alias ls='ls -aG --color=auto'
 end
 
-if test (uname) = "Darwin"
-  alias sudoedit="sudo -e"
+if test (uname) = Darwin
+    alias sudoedit="sudo -e"
 end
 
 # FZF
 if type -q fd
-  export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git --color=always'
+    export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git --color=always'
 end
 
 if type -q bat
-  export FZF_DEFAULT_OPTS='--ansi --preview "bat --color=always {}"'
+    export FZF_DEFAULT_OPTS='--ansi --preview "bat --color=always {}"'
 else
-  export FZF_DEFAULT_OPTS='--ansi'
+    export FZF_DEFAULT_OPTS='--ansi'
 end
 
 # FUNCTIONS
 
 # scan a doc like `scan foo.pdf`
 function scan
-  set temp (mktemp)
-  scanimage --device "airscan:w2:Brother ADS-2700W [000ec6eba5ad]" --format=pnm --output-file "$temp"
-  gm convert $temp $argv
+    set temp (mktemp)
+    scanimage --device "airscan:w2:Brother ADS-2700W [000ec6eba5ad]" --format=pnm --output-file "$temp"
+    gm convert $temp $argv
 end
 
 # open vi with an fzf autocomplete
 function fvi
-	vim (fzf)
+    vim (fzf)
 end
